08587749094b149c7bafbc4b968be8cec34bd592
Vaughn-Alston <vaughn2code@gmail.com>
2026-07-22 12:15:11 -0700

    Here I was able to get my screen working with shawn updates

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -7,17 +7,17 @@ import { useAuthentication } from "../../utils/hooks/useAuthentication";
 
 
 
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+// import * as React from 'react';
+// import Box from '@mui/material/Box';
+// import Drawer from '@mui/material/Drawer';
+// import List from '@mui/material/List';
+// import Divider from '@mui/material/Divider';
+// import ListItem from '@mui/material/ListItem';
+// import ListItemButton from '@mui/material/ListItemButton';
+// import ListItemIcon from '@mui/material/ListItemIcon';
+// import ListItemText from '@mui/material/ListItemText';
+// import InboxIcon from '@mui/icons-material/MoveToInbox';
+// import MailIcon from '@mui/icons-material/Mail';
 
 
 //import for a background
@@ -40,40 +40,7 @@ const handleSignOut = async () => {
 
 
 
-const [open, setOpen] = React.useState(false);
 
-const toggleDrawer = (newOpen) => () => {
-    setOpen(newOpen);
-  };
-  const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {['All mail', 'Trash', 'Spam'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </Box>
-  );
 
 
 
@@ -87,6 +54,7 @@ export default function ProfileScreen() {
   const navigation = useNavigation();
   const { user } = useAuthentication();
   const [astrology, setAstrology] = useState("Pisces");
+
   // const userSign = findAstrologySign();
 
   // (useEffect(() => {
@@ -96,9 +64,6 @@ export default function ProfileScreen() {
 
   return (
 
-
-    
-
     // Here will be the container that will hold the image and the icons
     <View style={styles.heroContainer}>
 
@@ -107,17 +72,6 @@ export default function ProfileScreen() {
     source={{ uri: "https://i.imgur.com/FxsJ3xy.jpg" }}
     style={styles.heroImage}
   />
-
-
-
-  <div>
-      <Button onClick={toggleDrawer(true)}>Open drawer</Button>
-      <Drawer open={open} onClose={toggleDrawer(false)}>
-        {DrawerList}
-      </Drawer>
-  </div>
-
-  
 
   {/* Settings button */}
   <View style={styles.settingsButton}>
